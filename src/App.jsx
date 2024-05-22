02543b581e68ee147ebfd661eabd926bbe992058
sryung <sryung1225@gmail.com>
2024-05-23 01:20:20 +0900

    [🎨 style] 모바일 기초 레이아웃 스타일 적용

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="app">
+    <div className="app w-full max-w-[430px] h-auto min-h-screen mx-auto bg-white shadow-md relative">
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/map" element={<Map />} /> */}
