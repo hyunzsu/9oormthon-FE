9b865191e04cb9fe18a8a4dd1dcb7b5eb980fdbe
hyunzsu <hyunzsu@naver.com>
2024-05-24 04:24:35 +0900

    [🎨 style] 예약성공 페이지 레이아웃

--- src/pages/UserSuccess.jsx
+++ src/pages/UserSuccess.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Button from '../components/Button'
 
 export default function UserSuccess() {
   const [reservationData, setReservationData] = useState(null)
@@ -13,43 +14,40 @@ export default function UserSuccess() {
   if (!reservationData) return <div>Loading...</div>
 
   return (
-    <section className="flex flex-col items-center p-6 bg-gray-50 rounded-lg shadow-lg max-w-md mx-auto">
+    <section className="flex flex-col items-center p-6 rounded-lg max-w-md mx-auto">
       <div className="text-center">
         <img
-          src="/src/assets/react.svg"
+          src="/src/assets/i-usersuccess.svg"
           alt="Success"
-          className="w-24 h-24 mb-4 mx-auto"
+          className="mx-auto mt-[90px] mb-[37px]"
         />
-        <h1 className="text-xl font-bold mb-2">예약이 완료되었습니다!</h1>
-        <p className="text-gray-600">메일로 예약 정보가 발송되었습니다.</p>
+        <h1 className="text-22 font-bold mb-2">예약이 완료되었습니다!</h1>
+        <p className="text-gray-600 text-14">
+          메일로 예약 정보가 발송되었습니다.
+        </p>
       </div>
-      <div className="bg-green-100 p-4 rounded-lg w-full mt-6">
-        <h2 className="text-lg font-semibold mb-4 text-gray-800">
+      <div className="bg-[#F4F4F4] p-[24px] rounded-lg w-full mt-[28px]">
+        <h2 className="text-16 font-semibold mb-4 text-gray-800">
           예약 정보 확인
         </h2>
-        <div className="space-y-2">
+        <div className="space-y-5">
           <div className="flex justify-between">
-            <span className="font-medium text-gray-700">날짜</span>
+            <span className="text-gray-700">예약자명</span>
+            <span className="text-gray-600">{reservationData.name}</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-gray-700">날짜 및 시간</span>
             <span className="text-gray-600">
               {reservationData.start} ~ {reservationData.end}
             </span>
           </div>
           <div className="flex justify-between">
-            <span className="font-medium text-gray-700">연락처</span>
-            <span className="text-gray-600">{reservationData.contact}</span>
-          </div>
-          <div className="flex justify-between">
-            <span className="font-medium text-gray-700">이메일</span>
+            <span className="text-gray-700">이메일</span>
             <span className="text-gray-600">{reservationData.email}</span>
-          </div>
-          <div className="flex justify-between gap-7">
-            <span className="font-medium text-gray-700 flex-shrink-0">
-              요청사항
-            </span>
-            <span className="text-gray-600">{reservationData.etc}</span>
           </div>
         </div>
       </div>
+      <Button text="참여자 오픈채팅방 참여하기" />
     </section>
   )
 }
