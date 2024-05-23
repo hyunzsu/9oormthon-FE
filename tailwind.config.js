23b1f9749c36c59e44efb58cf0d0870b03542fc3
hyunzsu <hyunzsu@naver.com>
2024-05-23 11:13:00 +0900

    [🔧 setting] tailwind.config 추가 (fontWeight, colors)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontWeight: {
+        regular: '400',
+        bold: '700',
+        semibold: '600',
+        medium: '500',
+        extrabold: '800',
+      },
+      colors: {
+        black: '#222222',
+        white: '#ffffff',
+      },
+    },
   },
   plugins: [],
-};
+}
