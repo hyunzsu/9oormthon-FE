df5b638cd26d1329ed741759561686891a99b9f5
hyunzsu <hyunzsu@naver.com>
2024-05-23 11:44:52 +0900

    [⚙️ chore] Home 페이지 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
-import DataTest from '../components/DataTest'
-import Products from '../components/products/Products'
-import Filter from '../components/products/Filter'
 
 export default function Home() {
   return (
     <div>
       <h1>Home 🏡</h1>
-      <DataTest />
-      <Filter />
-      <Products />
-      <button>내 공간 등록하기</button>
     </div>
   )
 }
