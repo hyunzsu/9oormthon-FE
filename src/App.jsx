81f0970ea15f61830429fddbeb214ce42b0fa9b9
hyunzsu <hyunzsu@naver.com>
2024-05-23 10:11:01 +0900

    [✨ feat] 상세페이지 라우터 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Detail from './pages/Detail'
 
 function App() {
   return (
     <div className="app w-full max-w-[430px] h-auto min-h-screen mx-auto bg-white shadow-md relative">
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/map" element={<Map />} /> */}
+        <Route path="/products/:id" element={<Detail />} />
       </Routes>
     </div>
   )
