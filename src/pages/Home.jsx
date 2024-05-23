52a2dbd073e8dd73660988306c737c6198ac97b8
sryung <sryung1225@gmail.com>
2024-05-24 06:38:05 +0900

    [✨ feat] 홈 스플래시

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home 🏡</h1>
-    </div>
+    <main
+      className="min-h-screen bg-cover bg-top"
+      style={{ backgroundImage: "url('/src/assets/splash.svg')" }}
+    >
+      <Link
+        to="/products"
+        className="fixed bottom-[34px] font-semibold text-black text-[16px] rounded-[30px] w-[calc(100%-80px)] max-w-[350px] h-[50px] mx-[40px] bg-white leading-[50px] text-center"
+      >
+        친구 만나러가기
+      </Link>
+    </main>
   )
 }
