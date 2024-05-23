aab3e9bd7683ac6e8b8c7772639bc36715b18760
hyunzsu <hyunzsu@naver.com>
2024-05-23 11:41:50 +0900

    [⚙️ chore] Space 페이지 생성 및 목록페이지 라우터 이동 (/products)

---
+++ src/pages/Space.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Products from '../components/products/Products'
+import Filter from '../components/products/Filter'
+
+export default function Space() {
+  return (
+    <div>
+      <Filter />
+      <Products />
+      <button>내 공간 등록하기</button>
+    </div>
+  )
+}
