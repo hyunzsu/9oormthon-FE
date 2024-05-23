c4f5b78a71e71d45edf7d80e119365a59281b9a4
sryung <sryung1225@gmail.com>
2024-05-24 02:58:21 +0900

    [🎨 style] 공통 타이틀 컴포넌스 스타일링

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,12 @@
 export default function Title({ mainText, subText }) {
   return (
-    <>
-      <h2 className="text-2xl">{mainText}</h2>
-      {subText && <h3 className="text-base pt-2">{subText}</h3>}
-    </>
+    <div className="my-[30px]">
+      <h2 className="text-[24px] leading-[30px] font-bold">{mainText}</h2>
+      {subText && (
+        <h3 className="text-base font-light pt-[10px] text-[#4A4A4A]">
+          {subText}
+        </h3>
+      )}
+    </div>
   )
 }
