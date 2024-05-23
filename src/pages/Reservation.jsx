d967e2d6b74598ba8ef33e9320b512c24d4c50d3
hyunzsu <hyunzsu@naver.com>
2024-05-24 04:23:53 +0900

    [✨ feat] 예약 페이지 기능 수정

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import DatePicker from 'react-datepicker'
 import { addDays } from 'date-fns'
 import 'react-datepicker/dist/react-datepicker.css'
-import '../styles/Reservation.css' // 커스텀 CSS 파일 import
 import BackButton from '../components/BackButton'
 import Button from '../components/Button'
 import {
@@ -22,19 +21,27 @@ export default function Reservation() {
 
   const navigate = useNavigate()
 
-  // const handleSubmit = () => {
-  //   // 서버에 데이터 전송하는 로직 추가
-  //   console.log('서버에 전송할 데이터:', formData)
+  const handleSubmit = () => {
+    const formData = {
+      name,
+      contact,
+      email,
+      etc,
+    }
+    // 서버에 데이터 전송하는 로직 추가
+    console.log('서버에 전송할 데이터:', formData)
 
-  //   // 응답이 성공적일 경우 로컬스토리지에 데이터 저장
-  //   localStorage.setItem('reservationData', JSON.stringify(formData))
-  //   navigate('/usersuccess') // 성공 페이지로 라우팅
-  // }
+    // 응답이 성공적일 경우 로컬스토리지에 데이터 저장
+    localStorage.setItem('reservationData', JSON.stringify(formData))
+    navigate('/usersuccess') // 성공 페이지로 라우팅
+  }
 
   return (
-    <div>
-      <h1 className="font-bold pt-5 pl-5">예약자 정보를 입력해주세요.</h1>
-      <div className="flex flex-col space-y-4 p-5">
+    <div className="p-[20px]">
+      <h1 className="font-bold text-24 pt-[58px] pb-[43px]">
+        예약자 정보를 입력해주세요.
+      </h1>
+      <div className="flex flex-col gap-[26px]">
         <InputText
           title="예약자"
           placeholder="현지수"
@@ -50,14 +57,14 @@ export default function Reservation() {
             placeholder="요청사항"
             value={etc}
             onChange={handleEtcChange}
-            className="border border-gray-300 rounded p-2 resize-none h-24"
+            className="border border-gray-300 rounded p-2 resize-none h-24 mt-[14px] w-full"
           ></textarea>
         </label>
       </div>
       <Button
         type="button"
-        // onClick={handleSubmit}
-        text="제출"
+        onClick={handleSubmit}
+        text="예약 완료하기"
         status={!name || !contact || !email ? 'disabled' : undefined}
       />
     </div>
