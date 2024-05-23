f66b49f0351bb6eb390817bb60faa2c5164827bc
sryung <sryung1225@gmail.com>
2024-05-23 15:18:40 +0900

    [✨ feat] 공간 사용 가능 시작일, 종료일 입력 달력

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,6 +3,9 @@ import Title from '../components/Title'
 import Button from '../components/Button'
 import { categories } from '../constants/categories'
 import { keywords } from '../constants/keywords'
+import DatePicker from 'react-datepicker'
+import { addDays, addMonths } from 'date-fns'
+import 'react-datepicker/dist/react-datepicker.css'
 
 // Step1 카테고리
 // Step2 키워드
@@ -18,7 +21,7 @@ const Step1 = ({ onNext }) => {
     onNext({ category })
   }
   return (
-    <section>
+    <section className="flex flex-col">
       <Title
         mainText={'어떤 목적의 공간인가요?'}
         subText={'게스트가 용도에 맞게 선택할 수 있어요'}
@@ -73,7 +76,7 @@ const Step2 = ({ onNext }) => {
     onNext({ keywords: selectedKeywords })
   }
   return (
-    <section>
+    <section className="flex flex-col">
       <Title
         mainText={'공간에 대해 알려주세요!'}
         subText={'공간을 빠르게 파악할 수 있어요. *중복선택가능'}
@@ -100,25 +103,42 @@ const Step2 = ({ onNext }) => {
   )
 }
 
-// const Step3 = ({ onNext }) => {
-//   const [date, setDate] = useState('')
-//   const handleNext = () => {
-//     onNext({ date })
-//   }
-//   return (
-//     <div>
-//       <h2>일멍님! 공간이 비어있는 기간을 알려주세요.</h2>
-//       <h3>대여 가능한 범위를 설정해주세요.</h3>
-//       <input type="date" value={date} onChange={e => setDate(e.target.value)} />
-//       <button onClick={handleNext}>다음</button>
-//     </div>
-//   )
-// }
+const Step3 = ({ onNext }) => {
+  const [startDate, setStartDate] = useState(null)
+  const [endDate, setEndDate] = useState(null)
+  const onChange = dates => {
+    const [start, end] = dates
+    setStartDate(start)
+    setEndDate(end)
+  }
+  const handleNext = () => {
+    onNext({ startDate, endDate })
+  }
+  return (
+    <section className="flex flex-col">
+      <Title
+        mainText={'일멍님! 공간이 비어있는 기간을 알려주세요.'}
+        subText={'대여 가능한 범위를 설정해주세요.'}
+      />
+      <DatePicker
+        selected={startDate}
+        onChange={onChange}
+        startDate={startDate}
+        endDate={endDate}
+        minDate={new Date()}
+        maxDate={addMonths(new Date(), 5)}
+        excludeDates={[addDays(new Date(), 1), addDays(new Date(), 5)]}
+        selectsRange
+        inline
+      />
+      <Button type="button" onClick={handleNext} text="다음" />
+    </section>
+  )
+}
 
 export default function Register() {
   const [step, setStep] = useState(1)
   const [formData, setFormData] = useState({})
-
   const handleNext = data => {
     setFormData(prevData => {
       const newData = { ...prevData, ...data }
@@ -126,11 +146,9 @@ export default function Register() {
     })
     setStep(step + 1)
   }
-
   const handleSubmit = () => {
     console.log(formData)
   }
-
   useEffect(() => {
     console.log('폼 업뎃!', formData)
   }, [formData])
@@ -139,7 +157,8 @@ export default function Register() {
     <div>
       {step === 1 && <Step1 onNext={handleNext} />}
       {step === 2 && <Step2 onNext={handleNext} />}
-      {step > 2 && <button onClick={handleSubmit}>제출</button>}
+      {step === 3 && <Step3 onNext={handleNext} />}
+      {step > 3 && <button onClick={handleSubmit}>제출</button>}
     </div>
   )
 }
