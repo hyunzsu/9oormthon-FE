47c0d3503c22fe393c2831657eb0d1bde772673c
hyunzsu <hyunzsu@naver.com>
2024-05-23 11:13:52 +0900

    [✨ feat] Filter 컴포넌트 생성

--- src/components/products/Filter.jsx
+++ src/components/products/Filter.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const categories = ['카페', '숙박시설', '컨퍼런스룸', '코워킹', '스튜디오']
 
 export default function Filter() {
   return (
-    <div>
-      <h2>filter</h2>
+    <div className="flex flex-wrap gap-4 mb-6 ml-4">
+      {categories.map(category => (
+        <button key={category}>{category}</button>
+      ))}
     </div>
   )
 }
