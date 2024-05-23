09c9acb9b7d3297c3a39a1c3873ee11be76ae2e5
sryung <sryung1225@gmail.com>
2024-05-24 08:58:18 +0900

    [🔧 setting] server url 지정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_SERVER_URL, // API의 기본 URL
+  baseURL: 'https://k8a8dc958104ca.user-app.krampoline.com/api', // API의 기본 URL
   timeout: 10000, // 요청 타임아웃 시간 설정 (밀리초)
   headers: {
     'Content-Type': 'application/json',
