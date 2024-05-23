da07737843c798c53b87ebe44aa64ac82e368ddf
sryung <sryung1225@gmail.com>
2024-05-24 02:57:52 +0900

    [⚙️ chore] 카테고리 상수 구성 변경

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,8 +1,26 @@
-export const categories = [
-  '카페',
-  '숙박시설',
-  '세미나',
-  '공유오피스',
-  '스튜디오',
-  '스터디룸',
-]
+export const categories = {
+  CAFE: {
+    name: '카페',
+    img: '/src/assets/i-cafe.svg',
+  },
+  B: {
+    name: '숙박시설',
+    img: '/src/assets/i-studio.svg',
+  },
+  C: {
+    name: '세미나',
+    img: '/src/assets/i-seminar.svg',
+  },
+  D: {
+    name: '공유오피스',
+    img: '/src/assets/i-office.svg',
+  },
+  E: {
+    name: '스튜디오',
+    img: '/src/assets/i-studio.svg',
+  },
+  F: {
+    name: '스터디룸',
+    img: '/src/assets/i-studyroom.svg',
+  },
+}
