4e97736f9ac813fafa3f320313ca7de47fabdaf8
hyunzsu <hyunzsu@naver.com>
2024-05-23 11:13:35 +0900

    [🎨 style] ProductCard 컴포넌트 레이아웃 수정

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -9,13 +9,18 @@ export default function ProductCard({ id }) {
   }
 
   return (
-    <div className="cursor-pointer" onClick={handleClick} className="flex">
+    <div
+      onClick={handleClick}
+      className="cursor-pointer flex justify-between w-[353px] h-[123px] border-2 border-black mx-auto my-0"
+    >
       <img src="/src/assets/react.svg" alt="상품 이미지" />
-      <div>
-        <h2>플레이스 캠프 제주</h2>
+      <div className="flex flex-col">
+        <h2 className="font-bold">플레이스 캠프 제주</h2>
         <p>활기차고 열정적인 분위기예요!</p>
+        <button className="border-gray-400 border-2 w-[90px] h-[26px] mt-auto ml-auto rounded-xl">
+          예약하기
+        </button>
       </div>
-      <button>예약하기</button>
     </div>
   )
 }
